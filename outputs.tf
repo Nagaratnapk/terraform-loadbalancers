3d47067a6f324df79228e2e8e13d42c8f670708c
Nagaratna Karetimmannanavar <nagaratnakaretimmannanavar@Nagaratnas-MBP.attlocal.net>
2022-11-11 19:08:14 -0800

    update

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,19 @@ output "PUBLIC_ALB_ARN" {
 
 output "PRIVATE_ALB_ARN" {
     value = module.alb-private.ALB_ARN
-}    
+}
+
+output "PRIVATE_LISTERNER_ARN" {
+    value = module.alb-private.LISTENER_ARN[0]
+}
+
+output "PRIVATE_ALB_ADDRESS" {
+    value =  module.alb-private.ALB_ADDRESS
+}
+
+output "PUBLIC_ALB_ADDRESS" {
+    value =  module.alb-public.ALB_ADDRESS
+}  
 
 
 
